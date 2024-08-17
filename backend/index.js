b3e1b81c1ab827d32fb69e8e238656d8337c5543
MukundChourasia <commnpara38@gmail.com>
2024-08-17 18:12:28 +0530

    Updated Index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,15 @@ const path = require("path");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+
+  origin: [''],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+
+}
+
+));
 
 //database connection with Mongodb//
 
